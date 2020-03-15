2e8ec1c4404d9f8f32efc7354590b2c3fd2f5dfa
bmalkmus <bmalkmus23@gmail.com>
2020-03-14 21:37:30 -0700

    added heroku database

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,20 @@ require('dotenv').config();
 
 console.log(process.env.PASSWORD)
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+};
+else {
+connection = mysql.createConnection({
     host : 'localhost',
     port: 3306,
     user: 'root',
     password: process.env.PASSWORDDB,
     database: 'burgers_db'
 })
+};
 
 connection.connect(function(err) {
     if (err) throw err;
