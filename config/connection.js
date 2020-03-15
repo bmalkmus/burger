2b55f7c87449aca4eec57384e4beecbd150507b9
bmalkmus <bmalkmus23@gmail.com>
2020-03-14 18:35:19 -0700

    activating servers

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
+console.log(process.env.PASSWORD)
 
 var connection = mysql.createConnection({
     host : 'localhost',
@@ -13,24 +16,5 @@ connection.connect(function(err) {
     console.log (`connected as id ${connection.threadID}`);
   });
 
-// var source = {
-//     localhost:{
-//         host: 'localhost',
-//         port: 3306,
-//         user: 'user',
-//         password: process.env.PASSWORD,
-//         database: 'burgers_db',
-//     }
-// };
-// console.log(source.localhost)
-// var connection = mysql.createConnection(source.localhost);
-
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error(" error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
 
 module.exports = connection;
