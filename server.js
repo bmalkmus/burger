ee45541b5271e2aa4c0941e17125ec15acd3857e
bmalkmus <bmalkmus23@gmail.com>
2020-04-07 21:28:41 -0700

    compression/performance

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require ('express');
+var compression = require ('compression');
 
 var app = express ();
 var PORT = process.env.PORT || 3333;
 
 app.use(express.static("public"));
+app.use(compression());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
